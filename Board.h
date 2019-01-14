bb224aa610bd17b5d5fddf3a769e34653e54b48a
delipl <delicat.kuba@gmail.com>
2019-01-14 19:44:20 +0100

    generowanie mapy, ustawienie pustych pol

--- Board.h
+++ Board.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "Board.cpp"
 
-int m = 10;
-int n = 10;
+int m = 17;       	//ilosc kolumn
+int n = 32;			//ilosc wierszy
 
 class Board{
 public:
@@ -17,26 +17,38 @@ public:
 };
 class Empty: public Board{
 public:
+	//name = "E";
 	Empty(){
-		printf("hello im empty\n");
+		this->name = 'E';
+	}
+};
+
+class noExist: public Board{
+public:
+	
+	noExist(){
+		this->name = ' ';
 	}
 };
 
 class Figure : public Board{
 public:
+	
 	bool owner;
 	char name;
-	void death(){
-		~Figure();
+	Figure(){	
+		this->name = 'F';
 	};
+	~Figure();
+
 };
 
 class Ghost: public Figure{
 public:
-	name = "G";
-	Ghost(int a, int b, /*bool player*/){
+	//name = "G";
+	Ghost(int a, int b/*, bool player*/){
 		this->x = a;
 		this->y = b;
 		//this->owner = player
 	};
-}
+};
--- test.cpp
+++ test.cpp
@@ -5,9 +5,18 @@
 int main(){
 	Board *board[m][n];                                          //dwuwymiarowa tablica obiektow
     for(int i = 0; i < m; i++){
-    	for (int j = 0; j < n; j++) board[i][j] = new Figure;
+    	for (int j = 0; j < n; j++) {
+        if(i%2 == 0 && j%2 == 0)      board[i][j] = new Empty;
+        else if(i%2 != 0 && j%2 != 0) board[i][j] = new Empty;
+        else                          board[i][j] = new noExist;
+      }
     }
-    
+    for(int i = 0; i < m; i++){
+      for (int j = 0; j < n; j++) {
+        printf("%c",board[i][j]->name);   
+      }
+      printf("\n");
+    }
   	//*board[0][0] = Empty();
   	//*board[0][1] = *board[0][0];
   	//*board[1][0] = Figure();
